e3520339716ca4024b488db23c09825e6616add6
unknown <federicofederico881@gmail.com>
2022-03-02 11:54:50 -0300

    removed commented code

--- contracts/ETHpool.sol
+++ contracts/ETHpool.sol
@@ -4,14 +4,15 @@ pragma solidity >=0.6.0;
 
 contract ETHPool{
 
+    address public owner;
     mapping (address => uint256) percentage;
+    Person[] participants;
     uint256 pool;
 
     struct Person{
         address receiver;
         uint256 deposit;
     }
-    Person[] participants;
 
     function depositToPool(address userReceiver, uint256 userDeposited) public{
         participants.push(Person(userReceiver, userDeposited));
@@ -20,6 +21,7 @@ contract ETHPool{
     }
 
     function depositRewards(uint256 reward) public{
+        require(msg.sender==owner);
         for(uint256 i=0;i<participants.length;i++){
             percentage[participants[i].receiver] = ((reward*participants[i].deposit)/pool)+participants[i].deposit;
         }
