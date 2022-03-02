a0d6e9c11ac2f02ef0492855fa59119db05de9eb
unknown <federicofederico881@gmail.com>
2022-03-02 11:37:02 -0300

    removed commented code

--- contracts/ETHpool.sol
+++ contracts/ETHpool.sol
@@ -4,42 +4,22 @@ pragma solidity >=0.6.0;
 
 contract ETHPool{
 
-    //mapping (address => uint256) deposit;
-    //uint256 rewardCount;
-
     mapping (address => uint256) percentage;
     uint256 pool;
 
     struct Person{
         address receiver;
         uint256 deposit;
-        //uint256 percentage;
     }
-
-   /* struct Percentage{
-        address person;
-        uint256 percentage;
-    }*/
-
-    /*struct RewardType{
-        uint256 rewardNumber;
-        Person[] participantsRecord;
-    }
-
-    //RewardType[] rewardhistory;*/
     Person[] participants;
 
     function depositToPool(address userReceiver, uint256 userDeposited) public{
-        //deposit[userReceiver] = userDeposited;
-        participants.push(Person(userReceiver, userDeposited/*,0*/));
+        participants.push(Person(userReceiver, userDeposited));
         percentage[userReceiver] = userDeposited;
         pool += userDeposited;
     }
 
     function depositRewards(uint256 reward) public{
-        //rewardCount += 1;
-        //rewardhistory.push(RewardType(rewardCount, participants));
-        
         for(uint256 i=0;i<participants.length;i++){
             percentage[participants[i].receiver] = ((reward*participants[i].deposit)/pool)+participants[i].deposit;
         }
