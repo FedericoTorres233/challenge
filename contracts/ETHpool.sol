6140c0775d9891290258c9d30553b2901d99dd9c
unknown <federicofederico881@gmail.com>
2022-03-04 02:26:11 -0300

    now contract handles duplicate deposits correctly

--- contracts/ETHpool.sol
+++ contracts/ETHpool.sol
@@ -7,12 +7,18 @@ contract ETHPool{
     event Log(uint256 amount);
     address private owner;
     mapping (address => uint256) percentage;
+    mapping (address => Repeated) repeated;
     Person[] participants;
     uint256 pool;
     event SendFunds(address from,address to,uint256 amount);
 
     constructor(){
         owner = msg.sender;
+    }
+
+    struct Repeated{
+        bool isRepeated;
+        uint256 accumulated;
     }
 
     struct Person{
@@ -24,21 +30,25 @@ contract ETHPool{
         participants.push(Person(msg.sender, userDeposited));
         percentage[msg.sender] += userDeposited;
         pool += userDeposited;
-        emit Log(pool);
     }
 
     function depositRewards(uint256 reward) payable public{
         require(msg.sender==owner,"Only the owner can deposit rewards");
-        for(uint256 a=0;a<participants.length;a++){
+        /*for(uint256 a=0;a<participants.length;a++){
             address part_addr = participants[a].receiver;
             if(percentage[part_addr] != participants[a].deposit){
                 participants[a].deposit = percentage[part_addr];
             }
-        }
+        }*/
 
         for(uint256 i=0;i<participants.length;i++){
             address part_addr = participants[i].receiver;
-            percentage[part_addr] = ((reward*participants[i].deposit)/pool)+participants[i].deposit;
+            if (!repeated[part_addr].isRepeated){
+                repeated[part_addr].isRepeated = true;
+                repeated[part_addr].accumulated += participants[i].deposit;
+            }
+            percentage[part_addr] += ((reward*repeated[part_addr].accumulated)/pool);
+            delete repeated[part_addr];
         }
         delete pool;
         delete participants;
@@ -46,6 +56,7 @@ contract ETHPool{
 
     function withdrawFunds() public {
         uint256 funds = percentage[msg.sender];
+        delete repeated[msg.sender];
         delete percentage[msg.sender];
         emit SendFunds(owner, msg.sender, funds);
     }
