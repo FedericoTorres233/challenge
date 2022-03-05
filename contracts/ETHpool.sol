a44c9c2ec6d84834c679fd4a87e5951f6f482358
FedericoTorres233 <federicofederico881@gmail.com>
2022-03-04 22:52:01 -0300

    add comments 💬💬

--- contracts/ETHpool.sol
+++ contracts/ETHpool.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.6.0;
 
 contract ETHpool {
+    // Declare variables, mappings, arrays and events
     address private owner;
     mapping(address => uint256) percentage;
     mapping(address => Repeated) repeated;
@@ -11,6 +12,7 @@ contract ETHpool {
     uint256 pool;
     event SendFunds(address from, address to, uint256 amount);
 
+    // Contract creator is the owner
     constructor() {
         owner = msg.sender;
     }
@@ -25,6 +27,7 @@ contract ETHpool {
         uint256 deposit;
     }
 
+    // Function that lets users to deposit to pool
     function depositToPool(uint256 userDeposited) public payable {
         participants.push(Person(msg.sender, userDeposited));
         percentage[msg.sender] += userDeposited;
@@ -32,11 +35,14 @@ contract ETHpool {
         pool_per_user[msg.sender] += userDeposited;
     }
 
+    // Function that lets the Team or owner to deposit rewards
     function depositRewards(uint256 reward) public payable {
         require(msg.sender == owner, "Only the owner can deposit rewards");
 
         for (uint256 i = 0; i < participants.length; i++) {
             address part_addr = participants[i].receiver;
+
+            // This if statement adds all one user's deposits in a single mapping
             if (!repeated[part_addr].isRepeated) {
                 repeated[part_addr].isRepeated = true;
                 repeated[part_addr].accumulated += participants[i].deposit;
@@ -50,7 +56,9 @@ contract ETHpool {
         delete participants;
     }
 
+    // Function that lets users to withdraw at any time and receive what the deserve
     function withdrawFunds() public {
+        // Remove the user from the contract
         for (uint256 i = 0; i < participants.length; i++) {
             if (participants[i].receiver == msg.sender) {
                 delete participants[i];
@@ -60,6 +68,7 @@ contract ETHpool {
         pool = pool - pool_per_user[msg.sender];
         delete pool_per_user[msg.sender];
         delete percentage[msg.sender];
+        // Send funds to user
         emit SendFunds(owner, msg.sender, funds);
     }
 }
