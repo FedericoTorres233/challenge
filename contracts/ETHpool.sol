c927287ac8eabe2acb7b0e3248264c6fa64d906f
FedericoTorres233 <federicofederico881@gmail.com>
2022-03-04 21:18:24 -0300

    contract passes all tests 🥳🥳

--- contracts/ETHpool.sol
+++ contracts/ETHpool.sol
@@ -2,12 +2,13 @@
 
 pragma solidity >=0.6.0;
 
-contract ETHPool{
+contract ETHpool{
 
     event Log(uint256 amount);
     address private owner;
     mapping (address => uint256) percentage;
     mapping (address => Repeated) repeated;
+    mapping (address => uint256) pool_per_user;
     Person[] participants;
     uint256 pool;
     event SendFunds(address from,address to,uint256 amount);
@@ -30,16 +31,11 @@ contract ETHPool{
         participants.push(Person(msg.sender, userDeposited));
         percentage[msg.sender] += userDeposited;
         pool += userDeposited;
+        pool_per_user[msg.sender] += userDeposited;
     }
 
     function depositRewards(uint256 reward) payable public{
         require(msg.sender==owner,"Only the owner can deposit rewards");
-        /*for(uint256 a=0;a<participants.length;a++){
-            address part_addr = participants[a].receiver;
-            if(percentage[part_addr] != participants[a].deposit){
-                participants[a].deposit = percentage[part_addr];
-            }
-        }*/
 
         for(uint256 i=0;i<participants.length;i++){
             address part_addr = participants[i].receiver;
@@ -49,14 +45,21 @@ contract ETHPool{
             }
             percentage[part_addr] += ((reward*repeated[part_addr].accumulated)/pool);
             delete repeated[part_addr];
+            delete pool_per_user[part_addr];
         }
         delete pool;
         delete participants;
     }
 
     function withdrawFunds() public {
+        for(uint256 i=0;i<participants.length;i++){
+            if (participants[i].receiver == msg.sender){
+                delete participants[i];
+            }
+        }
         uint256 funds = percentage[msg.sender];
-        delete repeated[msg.sender];
+        pool = pool - pool_per_user[msg.sender];
+        delete pool_per_user[msg.sender];
         delete percentage[msg.sender];
         emit SendFunds(owner, msg.sender, funds);
     }
