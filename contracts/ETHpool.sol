81116adc3a39915cd5853c68bc2efe4018713f84
FedericoTorres233 <federicofederico881@gmail.com>
2022-03-04 21:47:36 -0300

    remove log and format code ✨✨

--- contracts/ETHpool.sol
+++ contracts/ETHpool.sol
@@ -2,48 +2,47 @@
 
 pragma solidity >=0.6.0;
 
-contract ETHpool{
-
-    event Log(uint256 amount);
+contract ETHpool {
     address private owner;
-    mapping (address => uint256) percentage;
-    mapping (address => Repeated) repeated;
-    mapping (address => uint256) pool_per_user;
+    mapping(address => uint256) percentage;
+    mapping(address => Repeated) repeated;
+    mapping(address => uint256) pool_per_user;
     Person[] participants;
     uint256 pool;
-    event SendFunds(address from,address to,uint256 amount);
+    event SendFunds(address from, address to, uint256 amount);
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
     }
 
-    struct Repeated{
+    struct Repeated {
         bool isRepeated;
         uint256 accumulated;
     }
 
-    struct Person{
+    struct Person {
         address receiver;
         uint256 deposit;
     }
 
-    function depositToPool(uint256 userDeposited) payable public{
+    function depositToPool(uint256 userDeposited) public payable {
         participants.push(Person(msg.sender, userDeposited));
         percentage[msg.sender] += userDeposited;
         pool += userDeposited;
         pool_per_user[msg.sender] += userDeposited;
     }
 
-    function depositRewards(uint256 reward) payable public{
-        require(msg.sender==owner,"Only the owner can deposit rewards");
+    function depositRewards(uint256 reward) public payable {
+        require(msg.sender == owner, "Only the owner can deposit rewards");
 
-        for(uint256 i=0;i<participants.length;i++){
+        for (uint256 i = 0; i < participants.length; i++) {
             address part_addr = participants[i].receiver;
-            if (!repeated[part_addr].isRepeated){
+            if (!repeated[part_addr].isRepeated) {
                 repeated[part_addr].isRepeated = true;
                 repeated[part_addr].accumulated += participants[i].deposit;
             }
-            percentage[part_addr] += ((reward*repeated[part_addr].accumulated)/pool);
+            percentage[part_addr] += ((reward *
+                repeated[part_addr].accumulated) / pool);
             delete repeated[part_addr];
             delete pool_per_user[part_addr];
         }
@@ -52,8 +51,8 @@ contract ETHpool{
     }
 
     function withdrawFunds() public {
-        for(uint256 i=0;i<participants.length;i++){
-            if (participants[i].receiver == msg.sender){
+        for (uint256 i = 0; i < participants.length; i++) {
+            if (participants[i].receiver == msg.sender) {
                 delete participants[i];
             }
         }
